d47f4d5b9df42d28279e3cbb078865ad03318a1d
Lejou1488 <iwan-iwan01@mail.ru>
2019-11-28 10:49:54 +0300

    ноябрь

---
+++ РКиС 4/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace РКиС_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double res = 0;
+
+            double a = double.Parse(Console.ReadLine());
+            double c = double.Parse(Console.ReadLine());
+            double d = double.Parse(Console.ReadLine());
+
+            Primer();
+            Primer2(a, c, d, res);
+            Console.WriteLine(Inc(a, c, d, res));
+            Console.WriteLine(Inc(ref a, c, d, res));
+        }
+        public static void Primer()
+        {
+            double a = double.Parse(Console.ReadLine());
+            double c = double.Parse(Console.ReadLine());
+            double d = double.Parse(Console.ReadLine());
+            Console.WriteLine((c/d +Math.Log(3*a/2))/(c-a+1));
+        }
+        public static void Primer2(double a, double c, double d, double res)
+        {
+            res = (c / d + Math.Log(3 * a / 2)) / (c - a + 1);
+            Console.WriteLine(res);
+        }
+        
+        public static double Inc(double a, double c, double d, double res)
+        {
+            res = (c / d + Math.Log(3 * a / 2)) / (c - a + 1);
+            return res;
+        }
+        public static double Inc(ref double a, double c, double d, double res)
+        {
+            
+            res = (c / d + Math.Log(3 * a / 2)) / (c - a + 1);
+            return res;
+        }
+    }
+}
