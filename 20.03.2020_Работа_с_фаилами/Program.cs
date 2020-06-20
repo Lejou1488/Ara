a40138eb4baf9f31c72c71b655fc6f1fa11cf8a2
Lejou1488 <iwan-iwan01@mail.ru>
2020-06-20 20:04:34 +0300

    1

---
+++ 20.03.2020_Работа_с_фаилами/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+namespace _20._03._2020_Работа_с_фаилами
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a, b; //объявление переменных
+            Console.WriteLine("Введите логин");
+            a = Console.ReadLine(); //присвоение переменной того, что ввёл пользователь
+            Console.WriteLine("Введите пароль");
+            b = Console.ReadLine();
+            FileStream file1 = new FileStream(@"C:\Temp\aut.txt", FileMode.Create); //файловый поток
+            StreamWriter writer = new StreamWriter(file1); //потоковый писатель
+            writer.Write("Логин: "); //запись в файл
+            writer.Write(a);
+            writer.Write(" Пароль: ");
+            writer.Write(b);
+            writer.Close(); //закрытыие потока
+        }
+    }
+}
